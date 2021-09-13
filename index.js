cd5ee8890fe850400a9da2852a4b0b2c69b55333
ZuberDunge <iamZuberDunge@gmail.com>
2021-09-13 17:07:56 +0530

    update popup positon to fixed

--- index.js
+++ index.js
@@ -2,21 +2,17 @@ const checkButton = document.querySelector('#add-btn-popup');
 const boxHide = document.querySelector('#box-hide');
 const mainPage = document.querySelector('#main-page');
 const blurPage = document.querySelector('.blur-popup');
-const welcomeGif = document.querySelector('.welcome-gif');
+const addPopup = document.querySelector('.add-btn-popup');
 
 checkButton.addEventListener("click", () => {
 
     setTimeout(function(){  
          
-            blurPage.style.filter = `none`;
-            
-            welcomeGif.style.display=`none`;
+            blurPage.style.filter = `none`;   
      
 
-        
-}, 2000);
-boxHide.style.display = `none`;
-welcomeGif.style.display=`block`;
-// subheading.innerHTML="Welcome Groot! Hang on your phone is slow";
-
+            boxHide.style.display = `none`;
+            
+}, 1000);
+addPopup.innerText ="Almost there!"
 });
